cc737f6679bbda713de1e1a70e6c27210867f913
MavroFidi <thepixypoxy@gmail.com>
2025-11-23 19:06:34 +0530

    Refactor set operations to use set instead of dict

--- Sets.py
+++ Sets.py
@@ -1,30 +1,39 @@
 def union(x,y):
-  result = {}
-  for i in x:
-    result.add(i)
-  for j in y:
-    result.add(j)
+    result = []
+    result = set(result)
+    for i in x:
+      result.add(i)
+    for j in y:
+      result.add(j)
+    return result
 
 def intersect(x,y):
-  result = {}
-  for i in x:
-    for j in y:
-      if i == j:
-        result.add(i)
+    result = []
+    result = set(result)
+    for i in x:
+        for j in y:
+            if i == j:
+                result.add(i)
+    return result
 
 def diff(x,y):
-  result = {}
-  for i in y:
-    for j in x:
-      if i != j:
-        result.add(j)
+    result = []
+    result = set(result)
+    for i in x:
+        if i not in y:
+            result.add(i)
+    return result
 
 def symdiff(x,y):
-  result = {}
-  for i in x:
+    result = []
+    result = set(result)
+    for i in x:
+        if i not in y:
+            result.add(i)
     for j in y:
-      result.add(i)
-      result.add(j)
+        if j not in x:
+            result.add(j)
+    return result
 
 E = {0,2,4,6,8}
 N = {1,2,3,4,5}
